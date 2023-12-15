82d0478ae27a7b1c39f73ca29c02a724d2620bbb
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-15 22:33:13 +0300

    variables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee All TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput); // A
+
+userInput = "ABC";
+Console.WriteLine(userInput); // ABC
+
+int number;
+number = 3;
+Console.WriteLine(number); // 3
+
+int a = 1, b = 6;
+string name, lastName;
+
+
 Console.ReadKey();
-Console.WriteLine("ADR");
